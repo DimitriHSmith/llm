972388921339671736fe060d9239e6cc222a9296
DimitriHSmith <78492753+DimitriHSmith@users.noreply.github.com>
2024-03-24 22:29:57 -0400

    Update Chatbot.py

--- llm-examples/Chatbot.py
+++ llm-examples/Chatbot.py
@@ -12,8 +12,8 @@ import random
    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.title("💬 Fiserv")
+#st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hi! I'm Finserv your Financial Services Policy Expert. I can help you with summarising legislative documents, creating detailed study guides and exam material for staff training and ensure accurate conveyance of information."}]
 
